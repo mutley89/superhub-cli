335bb99ca011d4b74354e142f1e83cb032017661
Matthew Atkinson <mutley456@ntlworld.com>
2015-02-18 13:34:59 +0000

    Initial commit

---
+++ portforward.py
@@ -0,0 +1,143 @@
+import collections
+import re
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
+
+protocols = {"4": "TCP", "3": "UDP", "254": "TCP+UDP"}
+
+path = "/VmRgPortForwarding.html"
+post_path = "/cgi-bin/ForwardingCgi"
+
+class PortForwardError(Exception):
+    pass
+
+def portforward(opts):
+    url = opts.router_address + path
+    res = urllib.request.urlopen(url)
+    soup = BeautifulSoup(res.read())
+    rule_list = get_rules(soup)
+    opts.pf_command(rule_list, opts)
+    if opts.pf_command in [add_rule, delete_rule]:
+        send_rules(rule_list, soup, opts)
+
+def get_rules(soup):
+    """
+    returns a list of OrderedDicts containing the rules
+    """
+    # The router sends 20 groups of 7 <input type=hidden> elements, containing
+    # the currently set rules. It expects the names and values these back as 
+    # form data after any modifications have been made. It always sends and
+    # expects 20 goups regardless of how many rules are set. Unset rules have a
+    # name of ""
+    rule_list = []
+    for i in range(20):
+        rule = collections.OrderedDict()
+        for key in (
+                "Name", "StartPort", "EndPort", "Protocol", "IpAddr",
+                "Enable", "Delete"):
+            elem = soup.find("input", attrs = {
+                    "name":"VmPortForwardingRule" + key + str(i)})
+            rule[key] = elem["value"]
+        rule_list.append(rule)
+    return rule_list
+
+def send_rules(rule_list, soup, opts):
+    data = collections.OrderedDict()
+    for i, rule in enumerate(rule_list):
+        for key in rule:
+            data["VmPortForwardingRule" + key + str(i)] = rule[key]
+
+    # The same div that contains the rule <input> elements, contains another
+    # hidden <input> with a random id as the name attribute. The server
+    # expects this back as the name, with a value of 0, as part of the POST
+    # request. Neither the <div> nor the <input> have any id or identifying
+    # attributes, so just find a rule, then get the last input sibling.
+    a_rule = soup.find("input", attrs={"name": "VmPortForwardingRuleName1"})
+    data[a_rule.find_next_siblings("input")[-1]["name"]] = "0"
+    # Other data sent by the web client. Server sends no response whatsover if
+    # this isn't sent.
+    data["VmPortForwardingRestore"] = "0"
+
+    url = opts.router_address + post_path
+    data = urllib.parse.urlencode(data).encode("utf-8")
+    headers = {"Content-Type":"application/x-www-form-urlencoded"}
+    req = urllib.request.Request(url, data, headers=headers)
+    res = urllib.request.urlopen(req)
+    return res
+
+def list_rules(rule_list, opts):
+    row = "{:15} {:11} {:15} {:8} {:7}"
+    print(row.format("Name", "Ports", "IP Address", "Protocol", "Enabled"))
+    for rule in rule_list:
+        if rule["Name"] != "":
+            if rule["StartPort"] == rule["EndPort"]:
+                ports = rule["StartPort"]
+            else:
+                ports = "-".join(rule["StartPort"], rule["EndPort"])
+            gateway = re.search(
+                    r'^var gatewayIP = "([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)" ;',
+                    soup.text, re.M).group(1)
+            ip = gateway.split(".")
+            ip[3] = rule["IpAddr"]
+            ip = ".".join(ip)
+            protocol = protocols[rule["Protocol"]]
+            enabled = "yes" if rule["Enable"] == "1" else "no"
+            print(row.format(
+                    rule["Name"], ports, ip, protocol, enabled))
+
+def add_rule(rule_list, opts):
+    if opts.name is None:
+        # Set default rule name
+        opts.name = protocols[opts.protocol] + ":" + opts.ports[0]
+        if opts.ports[0] != opts.ports[1]:
+            opts.name += "-" + opts.ports[1]
+    # Check rule name doesn't already exist and any ports are not already
+    # forwarded
+    for rule in rule_list:
+        if rule["Name"].lower() == opts.name.lower():
+            raise PortForwardError(
+                    "Rule with name %s already exists" % opts.name)
+        if (rule["Protocol"] == "254" or opts.protocol == "254"
+                or rule["Protocol"] == opts.protocol == "254"):
+            if rule["Name"] != "":
+                # Need to check if router allows non enabled rules to overlap.
+                # The web interface js doesn't allow this, so do the same for
+                # now
+                is_forwarded = False
+                if (int(opts.ports[0]) <= int(rule["StartPort"]) and
+                        int(opts.ports[1]) >= int(rule["EndPort"])):
+                    is_forwarded = True
+                for port in opts.ports:
+                    if (int(port) >= int(rule["StartPort"]) and
+                            int(port) <= int(rule["EndPort"])):
+                        is_forwarded = True
+                if is_forwarded:
+                    raise PortForwardError("Port already forwarded")
+    # TODO: need to check address and gateway
+    gateway = re.search(
+            r'^var gatewayIP = "([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)" ;',
+            soup.text, re.M).group(1)
+    # Router doesn't allow the subnet to be anything other than a /24
+    if address.split(".")[:3] != opts.address.split(".")[:3]:
+        raise PortForwardError("Address must be on same subnet as gateway")
+    if opts.address == gateway:
+        raise PortForwardError("Address can not be gateway address")
+    # find first empty rule
+    for rule in rule_list:
+        if rule["Name"] == "":
+            break
+    rule["Name"] = opts.name
+    rule["StartPort"] = opts.ports[0]
+    rule["EndPort"] = opts.ports[1]
+    rule["IpAddr"] = opts.address
+    rule["Protocol"] = opts.protocol
+    rule["Enable"] = "1"
+
+def delete_rule(rule_list, opts):
+    for rule in rule_list:
+        if rule["Name"] == opts.name:
+            rule["Delete"] = "1"
+            break
+    else:
+        raise PortForwardError("Rule with name %s doesn't exist" % opts.name)
---
+++ superhub-cli.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import urllib.request
+import urllib.parse
+from getpass import getpass
+
+from bs4 import BeautifulSoup
+
+import portforward
+
+#import http.client
+#http.client.HTTPConnection.debuglevel = 0
+
+default_router_address = "192.168.0.1" 
+
+class LoginError(Exception):
+    pass
+
+def main():
+    opts = parse_args()
+    login(opts)
+    opts.command(opts)
+    if opts.logout:
+        logout(opts)
+
+def parse_args(cmdline=None):
+    parser = argparse.ArgumentParser(description="""CLI interface to the
+            Virgin media superhub router""")
+    parser.add_argument("-r", "--router-address", type=lambda x: "http://" + x,
+            default=default_router_address,
+            help="IP address and optional port of router")
+    parser.add_argument("-l", "--logout", action="store_true",
+            help="Logout when done")
+    subparsers = parser.add_subparsers()
+    pf_parser = subparsers.add_parser("portforward", aliases=["pf"],
+            description="Port forwarding") 
+    pf_parser.set_defaults(command=portforward.portforward)
+    pf_subparsers = pf_parser.add_subparsers()
+    pf_list_parser = pf_subparsers.add_parser("list",
+            description="List port forwarding rules")
+    pf_list_parser.set_defaults(pf_command=portforward.list_rules)
+    pf_add_parser = pf_subparsers.add_parser("add",
+            description="Add portforwarding rule")
+    def parse_ports(port_string):
+        res = port_string.split("-")
+        for x in res:
+            if not x.isnumeric():
+                raise argparse.ArgumentTypeError(
+                        "port must be a number, or 2 numbers seperated by -")
+            if int(x) < 1 or int(x) > 65535:
+                raise argparse.ArgumentTypeError(
+                        "ports must be between 1 and 65535")
+        if len(res) == 2:
+            if int(res[1]) < int(res[0]):
+                raise argparse.ArgumentTypeError(
+                        "Start port must not be higher than end port")
+        elif len(res) == 1:
+            res.append(res[0])
+        else:
+            raise argparse.ArgumentTypeError(
+                    "port must be a number, or 2 numbers seperated by -")
+        return res
+    pf_add_parser.set_defaults(protocol="254", pf_command=portforward.add_rule)
+    pf_add_parser.add_argument("ports", type=parse_ports,
+            help="Port to forward. Can be a number or range seperated by -")
+    pf_add_parser.add_argument("address",
+            help="Internal address to forward port to")
+    pf_add_parser.add_argument("--name", help="Name of Rule")
+    protocol_group = pf_add_parser.add_mutually_exclusive_group()
+    protocol_group.add_argument("--tcp", action="store_const", const="4",
+            dest="protocol", help="Forward tcp ports only")
+    protocol_group.add_argument("--udp", action="store_const", const="3",
+            dest="protocol", help="Forward udp ports only")
+    protocol_group.add_argument("--tcp-and-udp", action="store_const",
+            const="254", dest="protocol",
+            help="Forward both tcp and udp ports")
+    pf_delete_parser = pf_subparsers.add_parser("delete", aliases=["del"],
+            description="Delete Port forward rule")
+    pf_delete_parser.set_defaults(pf_command=portforward.delete_rule)
+    pf_delete_parser.add_argument("name", help="Name of rule to delete")
+    if cmdline is None:
+        return parser.parse_args()
+    else:
+        return parser.parse_args(cmdline)
+
+# Login seems to be done purely on the basis of ip address, once logged in, no
+# cookies or anything else is required to be sent
+def login(opts):
+    res = urllib.request.urlopen(opts.router_address + "/home.html")
+    if res.url == opts.router_address + "/home.html":
+        logged_in = True
+    else:
+        logged_in = False
+    password_regex = re.compile('^\tvar res="([^"]*)";', re.M)
+    page = res.read()
+    while not logged_in:
+        soup = BeautifulSoup(page)
+        password_name = soup.find("input", id="password")["name"]
+        password = getpass()
+        url = opts.router_address + "/cgi-bin/VmLoginCgi"
+        data = urllib.parse.urlencode({password_name: password}).encode("utf-8")
+        headers = {"Content-Type":"application/x-www-form-urlencoded"}
+        req = urllib.request.Request(url, data, headers=headers)
+        login_res = urllib.request.urlopen(req)
+        page = login_res.read()
+        page_str = page.decode("iso-8859-1")
+        regex_res = password_regex.search(page_str)
+        if regex_res.group(1) == "0":
+            logged_in = True
+        elif regex_res.group(1) == "1":
+            print("Incorrect password")
+        else:
+            raise LoginError
+
+def logout(opts):
+    urllib.request.urlopen(opts.router_address + "/VmLogout2.html")
+
+if __name__ == "__main__":
+    main()
